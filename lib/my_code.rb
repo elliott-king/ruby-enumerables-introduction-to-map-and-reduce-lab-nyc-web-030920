cde240bbad08f21322b72262032f9f05c41bede4
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-26 22:04:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # My Code here....
+def map_to_negativize source
+  ret = []
+  source.each do |val|
+    ret << val * -1
+  end
+  ret
+end
+
+def map_to_no_change source 
+  ret = []
+  source.each do |val|
+    ret << val
+  end
+  ret
+end
+
+def map_to_double source
+  ret = []
+  source.each do |val|
+    ret << val * 2
+  end
+  ret  
+end
+
+def map_to_square source
+  ret = []
+  source.each do |val|
+    ret << val * val
+  end
+  ret  
+end
